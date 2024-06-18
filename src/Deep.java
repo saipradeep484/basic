09172922338893ff60241551eb5b87574c45691c
saipradeep484 <saipradeep484@gmail.com>
2024-06-18 12:46:53 +0530

    first commit

---
+++ src/Deep.java
@@ -0,0 +1,15 @@
+public class Deep 
+{
+    static{
+        
+        System.out.println(true);
+
+    }
+
+    
+    public static void main(String [] arSt)
+    {
+        System.out.println(996);
+    }
+    
+}
